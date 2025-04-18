45774e4f4eac17ec1d7d2cf0c167ec67d48cedab
MidhunaMohanraj <97339582+MidhunaMohanraj@users.noreply.github.com>
2025-04-18 15:30:21 -0700

    Update server.c

--- server.c
+++ server.c
@@ -1,3 +1,5 @@
+// code template for a basic C server is from 
+// https://www.geeksforgeeks.org/socket-programming-cc/
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,21 +9,21 @@
 #define PORT 8080
 int main(int argc, char const* argv[])
 {
+    // declare variables for server socket, client socket, and address structures
     int server_fd, new_socket;
     ssize_t valread;
     struct sockaddr_in address;
     int opt = 1;
     socklen_t addrlen = sizeof(address);
     char buffer[1024] = { 0 };
-    // char* hello = "Hello from server";
 
-    // Creating socket file descriptor
+    // creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
+    // forcefully attaching socket to the port 8080
     if (setsockopt(server_fd, SOL_SOCKET,
                    SO_REUSEADDR | SO_REUSEPORT, &opt,
                    sizeof(opt))) {
@@ -32,17 +34,21 @@ int main(int argc, char const* argv[])
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 8080
+    // forcefully attaching socket to the port 8080
     if (bind(server_fd, (struct sockaddr*)&address,
              sizeof(address))
         < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
+
+    // listen on the port 8080 until we get a message
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
+
+    // if the socket gets a request to connect, accept it
     if ((new_socket
          = accept(server_fd, (struct sockaddr*)&address,
                   &addrlen))
@@ -51,15 +57,12 @@ int main(int argc, char const* argv[])
         exit(EXIT_FAILURE);
     }
   
-    // subtract 1 for the null
-    // terminator at the end
+    // receive the message from a client sender
     valread = read(new_socket, buffer,
                    1024 - 1); 
     printf("%s\n", buffer);
-    // send(new_socket, hello, strlen(hello), 0);
-    // printf("Hello message sent\n");
 
-    // closing the connected socket
+    // closing the connected socket after receiving one message
     close(new_socket);
   
     // closing the listening socket
