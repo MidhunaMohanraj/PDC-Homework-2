a4ff81a119cec7a8e7616dd5f965a3779761548e
MidhunaMohanraj <97339582+MidhunaMohanraj@users.noreply.github.com>
2025-04-18 15:16:22 -0700

    Update client.c

--- client.c
+++ client.c
@@ -1,27 +1,32 @@
+// code template for a basic C client is from 
+// https://www.geeksforgeeks.org/socket-programming-cc/
+// comments are by us
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 8080
+#define PORT 8080  // Port number to connect to
 
 int main(int argc, char const* argv[])
 {
     int status, valread, client_fd;
-    struct sockaddr_in serv_addr;
-    char* hello = "Hello from client";
-    char buffer[1024] = { 0 };
+    struct sockaddr_in serv_addr;       // struct to hold server address info
+    char* hello = "Hello from client"; // message to be sent to server
+    char buffer[1024] = { 0 };         // buffer to store server response
+    
+    //Create socket
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
     }
-
+    // Set up server address structure
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary
     // form
-    if (inet_pton(AF_INET, "192.168.100.4", &serv_addr.sin_addr)
+    if (inet_pton(AF_INET, "192.168.100.4", &serv_addr.sin_addr) // address of server
         <= 0) {
         printf(
             "\nInvalid address/ Address not supported \n");
